51941a1ee14ac59847a7cc3d17f3673683956681
Adrian Onica <>
2022-05-03 16:12:06 +0300

    Solved files issue

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.criminalintent
-
-import androidx.lifecycle.ViewModel
-
-class CrimeListViewModel : ViewModel() {
-
-    private val crimeRepository = CrimeRepository.get()
-    val crimeListLiveData = crimeRepository.getCrimes()
-}
