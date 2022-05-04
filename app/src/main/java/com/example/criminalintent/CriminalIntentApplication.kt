38bdf9b0663020c2ddaa0ff67e4ea672302c54ad
Adrian Onica <>
2022-05-04 16:32:26 +0300

    Change to id to UUID

--- app/src/main/java/com/example/criminalintent/CriminalIntentApplication.kt
+++ app/src/main/java/com/example/criminalintent/CriminalIntentApplication.kt
@@ -2,8 +2,6 @@ package com.example.criminalintent
 
 import android.app.Application
 import androidx.room.Room
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.criminalintent.data.local.CrimeDatabase
 import com.example.criminalintent.data.repository.CrimeRepository
 
@@ -13,31 +11,7 @@ class CriminalIntentApplication : Application() {
         super.onCreate()
         val database =
             Room.databaseBuilder(this, CrimeDatabase::class.java, CrimeRepository.DATABASE_NAME)
-                .addMigrations(migration()).build()
+                .build()
         CrimeRepository.initialize(database.crimeDao())
-    }
-
-    private fun migration(): Migration {
-        val migration = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL(
-                    "CREATE TABLE new_table ( id INTEGER NOT NULL," +
-                            "title TEXT NOT NULL," +
-                            "date INTEGER NOT NULL, " +
-                            "isSolved INTEGER NOT NULL," +
-                            "PRIMARY KEY(id))"
-                )
-                database.execSQL(
-                    "INSERT INTO new_table(" +
-                            "title," +
-                            "date," +
-                            "isSolved)" +
-                            " SELECT title, date, isSolved FROM Crime"
-                )
-                database.execSQL("DROP TABLE Crime")
-                database.execSQL("ALTER TABLE new_table RENAME TO Crime")
-            }
-        }
-        return migration
     }
 }
