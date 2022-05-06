755507f338f6418bc2f7320308dd02b7e48d7397
Adrian Onica <>
2022-05-06 10:43:47 +0300

    Fixed syntax issue

--- app/src/main/java/com/example/criminalintent/presentation/dialogs/DatePickerFragment.kt
+++ app/src/main/java/com/example/criminalintent/presentation/dialogs/DatePickerFragment.kt
@@ -4,31 +4,42 @@ import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.widget.DatePicker
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
-import java.util.*
+import java.util.Calendar
+import java.util.Date
+import java.util.GregorianCalendar
+
 
 class DatePickerFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dateListener =
-            DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, day: Int ->
-                val resultDate: Date = GregorianCalendar(year, month, day).time
-                val result = Bundle().apply {
-                    putSerializable(RESULT_DATE_KEY, resultDate)
-                }
-                val resultRequestCode = requireArguments().getString(ARG_REQUEST_CODE, "")
+        val date = getInitialDateCalendar()
+        val args = arguments?.getSerializable(ARG_DATE) as Date
+        date.time = args
+        return DatePickerDialog(
+            requireContext(), getDateListener(), date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH)
+        )
+    }
+
+    private fun getDateListener(): DatePickerDialog.OnDateSetListener {
+        return DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, day: Int ->
+            val resultDate: Date = GregorianCalendar(year, month, day).time
+            val result = bundleOf(RESULT_DATE_KEY to resultDate)
+            val resultRequestCode = requireArguments().getString(ARG_REQUEST_CODE, "")
+            if (isAdded) {
                 parentFragmentManager.setFragmentResult(resultRequestCode, result)
             }
+        }
+    }
 
-        val date = arguments?.getSerializable(ARG_DATE) as Date
+    private fun getInitialDateCalendar(): Calendar {
         val calendar = Calendar.getInstance()
-        calendar.time = date
-        val initialYear = calendar.get(Calendar.YEAR)
-        val initialMonth = calendar.get(Calendar.MONTH)
-        val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
-        return DatePickerDialog(
-            requireContext(), dateListener, initialYear, initialMonth, initialDay
-        )
+        return calendar.apply {
+            this.get(Calendar.YEAR)
+            this.get(Calendar.MONTH)
+            this.get(Calendar.DAY_OF_MONTH)
+        }
     }
 
     companion object {
@@ -37,11 +48,10 @@ class DatePickerFragment : DialogFragment() {
         private const val RESULT_DATE_KEY = "dateKey"
 
         fun newInstance(date: Date, requestCode: String): DatePickerFragment {
-            val args = Bundle().apply {
-                putSerializable(ARG_DATE, date)
-                putString(ARG_REQUEST_CODE, requestCode)
-
-            }
+            val args = bundleOf(
+                ARG_DATE to date,
+                ARG_REQUEST_CODE to requestCode
+            )
             return DatePickerFragment().apply {
                 arguments = args
             }
